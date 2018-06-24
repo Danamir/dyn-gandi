01dc61b99fa30dea959624669804cecb02e047c0
Danamir <danamir@danamir.com>
2018-06-25 01:31:29 +0200

    Python package setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'docopt',
+    'requests',
+]
+
+setup(
+    name='dyn-gandi',
+    version='1.0',
+    long_description=__doc__,
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    entry_points={
+        'console_scripts': [
+        ]
+    },
+    dependency_links=[]
+)
